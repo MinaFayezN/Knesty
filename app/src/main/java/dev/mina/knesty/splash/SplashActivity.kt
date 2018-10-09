8000e5da7ddd29856b43adfddd4e59684375f88e
Mina Fayez <Mina.Fayez@protonmail.com>
2018-10-09 17:19:32 +0200

    Add Custom Font Add SVG Icon Add Path Animator Lib

---
+++ app/src/main/java/dev/mina/knesty/splash/SplashActivity.kt
@@ -0,0 +1,30 @@
+package dev.mina.knesty.splash
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.animation.AccelerateDecelerateInterpolator
+import com.eftimoff.androipathview.PathView
+import dev.mina.knesty.R
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        initializePathView()
+    }
+
+    private fun initializePathView() {
+        val pathView = findViewById<PathView>(R.id.splash_logo_pathView)
+        pathView.useNaturalColors()
+//        pathView.setFillAfter(true)
+        pathView.pathAnimator
+                .delay(100)
+                .duration(2000)
+                .listenerStart { }
+                .listenerEnd { }
+                .interpolator(AccelerateDecelerateInterpolator())
+                .start()
+    }
+}
